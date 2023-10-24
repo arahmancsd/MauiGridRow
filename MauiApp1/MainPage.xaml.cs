f483c9d1f4534faddf1bb279e008ee9aef6dc0f4
Abdul Rahman Hamidy <arahmancsd@gmail.com>
2023-10-24 21:04:28 +0100

    Maui Grid Row Test Project

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		BindingContext = new MainPageViewModel();
+	}
+}
+
+
+public sealed partial class MainPageViewModel : ObservableObject
+{
+	public ObservableCollection<Person> People { get; private set; } = [];
+
+	public MainPageViewModel()
+	{
+		AddPersons();
+	}
+
+	private void AddPersons()
+	{
+		for (int index = 1; index < 100; index++)
+		{
+			People.Add(new Person() { Id = index, Name = $"Random Person {index}" });
+		}
+	}
+}
+
+
+public sealed record Person
+{
+	public int Id { get; init; }
+	public string Name { get; init; }
+}
---
+++ MauiApp1/Page1.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
